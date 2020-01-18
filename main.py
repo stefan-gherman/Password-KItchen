0cf050eb76c3232cb2cdb4c7dd04432170cf4b79
Stefan Gherman <scgheman@gmail.com>
2020-01-19 00:47:06 +0200

    Remove global variable 'password'.

--- main.py
+++ main.py
@@ -2,23 +2,30 @@ from flask import Flask, redirect, request, render_template, url_for
 import hasher
 app = Flask(__name__)
 
-password = None
+
 @app.route('/', methods=['GET', 'POST'])
 def return_home():
-    global password
-    password=None
     return render_template("index.html", status="default")
 
 
 @app.route('/hash', methods=['GET', 'POST'])
 def hash_password():
     if request.method == 'POST':
-        global password
         password = request.form['password']
         hashed_password = hasher.hash_pw(password)
-        print("This is the password: ", password)
+        print(hashed_password)
         return render_template("index.html", status="default", show_hash=hashed_password, password=password)
 
-
+@app.route('/dehash', methods = ['GET', 'POST'])
+def check_hash():
+    if request.method == 'POST':
+        password = request.form['password_verif']
+        hashed = request.form['hash_verif']
+        truth = hasher.check_pw(password, hashed)
+        print(truth)
+        if truth is True:
+            return render_template("index.html", status="checked", hash=hashed, password_verif=password)
+        else:
+            return render_template("index.html", status="failed", hash=hashed, password_verif=password)
 if __name__ == "__main__":
     app.run(debug=True)
