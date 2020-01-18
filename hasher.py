4a67a1dde4f2749e88fec24b1f09922515c7808c
Stefan Gherman <scgheman@gmail.com>
2020-01-18 23:40:25 +0200

    Add hashing functions to hasher.py module.

---
+++ hasher.py
@@ -0,0 +1,10 @@
+import bcrypt
+
+def hash_pw(plain_text):
+    hashed_bytes = bcrypt.hashpw(plain_text.encode('utf-8'), bcrypt.gensalt())
+    return hashed_bytes.decode('utf-8')
+
+
+def check_pw(plain_text, hashed_pw):
+    hashed_pw_encoded = hashed_pw.encode('utf-8')
+    return bcrypt.checkpw(plain_text, hashed_pw_encoded)
